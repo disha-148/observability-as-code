f3843651f96bbb111bd55afc6a8d82f97bf28705
Disha <disha.14624@gmail.com>
2026-06-22 12:35:05 -0400

    Delete tools/fleet/dist/index.js

--- tools/fleet/dist/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cli_1 = require("./cli");
-const restart_1 = require("./handlers/restart");
-// Wire CLI commands
-(0, cli_1.configureCLI)({
-    handleRestart: restart_1.handleRestart
-});
